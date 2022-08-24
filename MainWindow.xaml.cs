712ff1fa0a6b1563855c6e5c326d7805640109d6
FateMorgan <fatemorgan@yandex.com>
2022-08-24 22:25:26 +0300

    Added an Sqlite module, some tables, sketched DB structure

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
 
 namespace Apricat
 {
@@ -23,6 +24,62 @@ namespace Apricat
         public MainWindow()
         {
             InitializeComponent();
+
+            string connectionString = "Data Source=lessons.db";
+            string sqlExpression = @"";
+
+            using (SqliteConnection connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+
+                SqliteCommand command = new SqliteCommand(sqlExpression, connection);
+                try
+                {
+                    command.ExecuteNonQuery();
+                    MessageBox.Show("Table was successfully created");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+        void settingsButton_GroupBoxCollapse(object sender, RoutedEventArgs e)
+        {
+            if (settingsGroupBox.Visibility == Visibility.Collapsed)
+                settingsGroupBox.Visibility = Visibility.Visible;
+            else settingsGroupBox.Visibility = Visibility.Collapsed;
+        }
+        private abstract class Lesson
+        {
+            public string Id { get; set; }
+            public string KeyWord { get; set; }
+            public string Level { get; set; }
+
+            public void ShowWord() { }
+            public void ControlWord() { }
+            public void MarkLearned() { }
+        }
+        private class WordLesson : Lesson
+        {
+            
+        }
+        private class SentenceLesson : Lesson
+        {
+
+        }
+        private class GrammarLessson : Lesson
+        {
+
+        }
+        private class ExcerciseLesson : Lesson
+        {
+
+        }
+        private class AdviceLesson : Lesson
+        {
+
         }
     }
 }
